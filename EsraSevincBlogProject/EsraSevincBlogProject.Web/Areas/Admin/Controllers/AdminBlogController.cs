5cf9522c513b4f02992f3cd9be103b82d2ba2ed3
esrasevinc <82102582+esrasevinc@users.noreply.github.com>
2023-06-12 16:38:58 +0300

    Admin Controllerlar eklendi
    Admin Controllerlar eklendi

---
+++ EsraSevincBlogProject/EsraSevincBlogProject.Web/Areas/Admin/Controllers/AdminBlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EsraSevincBlogProject.Web.Areas.Admin.Controllers
+{
+	public class AdminBlogController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
