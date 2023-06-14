b20d05966e9bbfd794e9cfe4a20180d206b84550
esrasevinc <82102582+esrasevinc@users.noreply.github.com>
2023-06-14 15:37:01 +0300

    Kategori ve Blog Admin sayfaları çalışmaları
    Kategori ve Blog Admin sayfaları çalışmaları

--- EsraSevincBlogProject/EsraSevincBlogProject.Web/Areas/Admin/Controllers/AdminBlogController.cs
+++ EsraSevincBlogProject/EsraSevincBlogProject.Web/Areas/Admin/Controllers/AdminBlogController.cs
@@ -1,12 +1,61 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EsraSevincBlogProject.Business.Abstract;
+using EsraSevincBlogProject.Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EsraSevincBlogProject.Web.Areas.Admin.Controllers
 {
-	public class AdminBlogController : Controller
+    [Area("Admin")]
+    public class AdminBlogController : Controller
 	{
-		public IActionResult Index()
+        IBlogService _blogService;
+
+        public AdminBlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+        public IActionResult Index()
 		{
-			return View();
-		}
-	}
+            List<Blog> liste = _blogService.GetAll();
+            return View(liste);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Blog p1)
+        {
+            p1.CreateTime = DateTime.Now.ToLongDateString();
+            int result = _blogService.Insert(p1);
+            return result == 0 ? View(p1) : RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Blog detail = _blogService.GetById(id);
+            detail.DeleteTime = DateTime.Now.ToLongDateString();
+            int result = _blogService.Delete(detail);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            Blog detail = _blogService.GetById(id);
+            detail.UpdateTime = DateTime.Now.ToLongDateString();
+            return View(detail);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Blog p1)
+        {
+            p1.UpdateTime = DateTime.Now.ToLongDateString();
+            int result = _blogService.Update(p1);
+            return result == 0 ? View(p1) : RedirectToAction("Index");
+        }
+
+    }
 }
--- EsraSevincBlogProject/EsraSevincBlogProject.Web/Areas/Admin/Controllers/AdminCategoryController.cs
+++ EsraSevincBlogProject/EsraSevincBlogProject.Web/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -1,12 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EsraSevincBlogProject.Business.Abstract;
+using EsraSevincBlogProject.Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EsraSevincBlogProject.Web.Areas.Admin.Controllers
 {
-	public class AdminCategoryController : Controller
+    [Area("Admin")]
+    public class AdminCategoryController : Controller
 	{
-		public IActionResult Index()
+        ICategoryService _categoryService;
+
+        public AdminCategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
 		{
-			return View();
-		}
-	}
+            List<Category> liste = _categoryService.GetAll();
+            return View(liste);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Category p1)
+        {
+            p1.CreateTime = DateTime.Now.ToLongDateString();
+            int result = _categoryService.Insert(p1);
+            return result == 0 ? View(p1) : RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Category detail = _categoryService.GetById(id);
+            detail.DeleteTime = DateTime.Now.ToLongDateString();
+            int result = _categoryService.Delete(detail);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id)
+        {
+            Category detail = _categoryService.GetById(id);
+            detail.UpdateTime = DateTime.Now.ToLongDateString();
+            return View(detail);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Category p1)
+        {
+            p1.UpdateTime = DateTime.Now.ToLongDateString();
+            int result = _categoryService.Update(p1);
+            return result == 0 ? View(p1) : RedirectToAction("Index");
+        }
+    }
 }
